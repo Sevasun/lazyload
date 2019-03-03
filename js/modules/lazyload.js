9c690dc2d2d3a50352558f3cd525fb944d1a54b0
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-03-03 19:41:38 +0200

    add load all recursion version

--- js/modules/lazyload.js
+++ js/modules/lazyload.js
@@ -17,23 +17,36 @@ function LazyLoad(options) {
                 let address = item.getAttribute(self.settings.sourceAttr);
                 if(item.tagName === "IMG") {
                     item.setAttribute('src', address);
+                    item.removeAttribute(self.settings.sourceAttr);
                     item.addEventListener('load', () => {
                         resolve();
                     });
                 } else {
                     item.style.backgroundImage = `url('${address}')`;
+                    item.removeAttribute(self.settings.sourceAttr);
                     resolve();
                 }
             });
         }
     }
 
+    // loop version
     async function loadAll(list) {
         for(let i = 0; i < list.length;) {
             await loadImg(list[i]);
             i++;
         }
     }
+
+    // recursion version
+    // async function loadAll(list) {
+    //     list = Array.from(list);
+    //     if(list.length > 0) {
+    //         await loadImg(list[0]);
+    //         list.shift();
+    //         loadAll(list);
+    //     }
+    // }
 
     if(this.settings.loadType === 'all') {
         loadAll(lazyItems);
