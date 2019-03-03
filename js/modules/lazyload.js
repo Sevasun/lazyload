1bb5bb3e7bfefe57ae8400a05dda57b1c15cf4e5
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-03-03 02:25:56 +0200

    load all in queue ready

--- js/modules/lazyload.js
+++ js/modules/lazyload.js
@@ -10,32 +10,6 @@ function LazyLoad(options) {
     // this.sourceAttr = this.settings.sourceAttr;
     
     let lazyItems = document.querySelectorAll(`[${this.settings.sourceAttr}]`);
-    // let img = [];
-    // let bg = [];
-    // function addSrc(element) {
-    //     let address = element.getAttribute(self.settings.sourceAttr);
-        
-    //     return new Promise ((resolve) => {
-    //         setTimeout(() => {
-    //             console.log('1');
-    //             element.setAttribute('src', address);
-    //         }, 1000);
-    //     });
-    // };
-
-    // for(let i = 0; i < lazyItems.length; i++) {
-    //     if(lazyItems[i].tagName === "IMG") {
-    //         addSrc(lazyItems[i]);
-    //     }
-    // }; 
-    // function getAddress() {
-    //     let address = self.getAttribute(self.settings.sourceAttr);
-    //     return address;
-    // }
-
-    // function setSrc(addr) {
-    //     self.setAttribute('src', addr);
-    // }
 
     function loadImg(item) {
         if(item.getAttribute(self.settings.sourceAttr)) {
@@ -43,27 +17,27 @@ function LazyLoad(options) {
                 let address = item.getAttribute(self.settings.sourceAttr);
                 if(item.tagName === "IMG") {
                     item.setAttribute('src', address);
+                    item.addEventListener('load', () => {
+                        resolve();
+                    });
                 } else {
                     item.style.backgroundImage = `url('${address}')`;
+                    resolve();
                 }
-                resolve();
             });
         }
     }
 
-    // if(this.settings.loadType === 'all') {
-    //     let i = 0;
-    //     // for(let i =0; i < lazyItems.length; i++) {
-    //     //     loadImg(lazyItems[i]);
-    //     // }
-    //     while(i < lazyItems.length) {
-    //         // loadImg(lazyItems[i]);
-    //         // lazyItems[i].addEventListener('change', () => {
-    //         //     console.log(i);
-    //         //     i++;
-    //         // });
-    //     }
-    // }
+    async function loadAll(list) {
+        for(let i = 0; i < list.length;) {
+            await loadImg(list[i]);
+            i++;
+        }
+    }
+
+    if(this.settings.loadType === 'all') {
+        loadAll(lazyItems);
+    }
 };
 
 export default LazyLoad;
